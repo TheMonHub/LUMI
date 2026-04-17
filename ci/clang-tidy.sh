2d1215c0dad015df4ce16911b4b4e22458c82fdd
TheMonHub <mono@themonhub.net>
2026-04-18 00:46:25 +0700

    fix ci

--- ci/clang-tidy.sh
+++ ci/clang-tidy.sh
@@ -2,5 +2,13 @@
 set -e
 
 cd "$(dirname "$0")"/.. || error_exit "Failed to change directory"
+
+echo "Installing LuaJIT"
+git clone https://luajit.org/git/luajit.git
+cd luajit
+git checkout v2.1
+make && sudo make install
+cd ..
+
 echo "Running clang-tidy checks..."
 ./automated/check.sh
